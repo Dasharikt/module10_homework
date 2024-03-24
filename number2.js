165ad27ec89ce3e4f48f85c869889be2d4e00fc4
Dasharikt <160501771+Dasharikt@users.noreply.github.com>
2024-03-24 16:43:55 +0300

    Add files via upload

---
+++ number2.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+Created using JS Bin
+http://jsbin.com
+
+Copyright (c) 2024 by anonymous (http://jsbin.com/fazabaloya/1/edit)
+
+Released under the MIT license: http://jsbin.mit-license.org
+-->
+<meta name="robots" content="noindex">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+
+<script id="jsbin-javascript">
+x =
+b = typeof x;
+switch (b) {
+  case "number" :
+    console.log("x-число");
+    break;
+  case "string" :
+    console.log("");
+    break;
+}
+
+
+</script>
+</body>
+</html>
