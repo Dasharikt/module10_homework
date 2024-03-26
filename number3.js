9fca1a9e44a6a8594363284b9b714d592fff5b38
Dasharikt <160501771+Dasharikt@users.noreply.github.com>
2024-03-26 11:45:42 +0300

    number3.js

--- number3.js
+++ number3.js
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<!--
-Created using JS Bin
-http://jsbin.com
 
-Copyright (c) 2024 by anonymous (http://jsbin.com/fazabaloya/1/edit)
-
-Released under the MIT license: http://jsbin.mit-license.org
--->
-<meta name="robots" content="noindex">
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>JS Bin</title>
-</head>
-<body>
-
-<script id="jsbin-javascript">
 function reverseString(str) {    
     var splitString = str.split("");    
     var reverseArray = splitString.reverse();          
@@ -27,7 +9,3 @@ function reverseString(str) {
 } 
 reverseString("goodbye");
 
-
-</script>
-</body>
-</html>
