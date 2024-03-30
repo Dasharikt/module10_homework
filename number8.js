a5e7708c80a764f27da79614bdd837daf74f28c0
Dasharikt <160501771+Dasharikt@users.noreply.github.com>
2024-03-30 14:37:08 +0300

    number8.js

---
+++ number8.js
@@ -0,0 +1,10 @@
+fam = new  Map ([
+  ['Mother','Sveta'],
+  ['Father','Tolya'],
+  ['Daughter','Dasha'],
+  ['Cat','Pushok']
+])
+
+for (let [key, value] of fam) {
+  console.log('Ключ -' + key + ',' + 'Значение -' + value)
+}
